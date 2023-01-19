69db1120d0c9fcc0e5491e9e32a3bbe4dbf5ca29
Gary Blankenship <brujah@gmail.com>
2023-01-18 21:30:49 -0500

    add totalAssets() method, and config for cardanodb.database.connection env var

--- src/CardanoWallet.php
+++ src/CardanoWallet.php
@@ -6,7 +6,12 @@ use Illuminate\Support\Facades\DB;
 
 class CardanoWallet
 {
-    private string $address;
+    public string $address;
+    public string $stake;
+
+    public string $connection;
+    public float $balance;
+    public int $totalAssets;
 
     /**
      * @param $address
@@ -14,16 +19,32 @@ class CardanoWallet
     public function __construct($address)
     {
         $this->address = $address;
+        $this->stake   = stake_address($address);
+
+        $this->connection = config('cardanodb.database.connection');
+
+        //$this->balance     = $this->balance();
+        //$this->totalAssets = $this->totalAssets();
+    }
+
+    public function totalAssets()
+    {
+        $this->totalAssets = DB::connection($this->connection)
+                               ->selectOne('SELECT COUNT(*) as total FROM assets_at_address(?)', [$this->stake])->total;
+
+        return $this->totalAssets;
     }
 
     public function balance()
     {
-        $lovelace = DB::connection(config('cardanodb.database.connection'))
+        $lovelace = DB::connection($this->connection)
                       ->table('utxo_view')
                       ->join('stake_address', 'stake_address.id', '=', 'utxo_view.stake_address_id')
                       ->where('view', $this->address)
                       ->sum('value');
 
-        return floatval($lovelace / 1000000);
+        $this->balance = floatval($lovelace / 1000000);
+
+        return $this->balance;
     }
 }
